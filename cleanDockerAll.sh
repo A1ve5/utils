7947c017406140a16a26db408aaa9b74a4d9c5e9
Luís Alves <lalves@lip.pt>
2017-07-25 17:30:09 +0100

    adding stop before all

--- cleanDockerAll.sh
+++ cleanDockerAll.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
-
+#
 # Shamelessly copied from https://techoverflow.net/2013/10/22/docker-remove-all-images-and-containers/
+#
 
-##
+
+# Stop all containers
+docker stop $(docker ps -q)
 
 # Delete all containers
 docker rm $(docker ps -a -q)
